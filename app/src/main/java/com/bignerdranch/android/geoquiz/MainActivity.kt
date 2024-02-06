94a6465489c26603b7b0fc3a747a2094257b8d5c
ttaliacc <taliac@bu.edu>
2024-02-06 13:23:32 -0500

    migrate snackbar files

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.bignerdranch.android.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+            Snackbar.make(
+                view,
+                R.string.correct_toast,
+                Snackbar.LENGTH_LONG)
+                .show()
+        }
+        falseButton.setOnClickListener { view: View ->
+            Snackbar.make(
+                view,
+                R.string.incorrect_toast,
+                Snackbar.LENGTH_LONG)
+                .show()
+        }
+    }
+}
